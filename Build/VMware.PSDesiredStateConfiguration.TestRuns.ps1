8164b4ff64feb0b6b4b8e3fe85c904d9c973543d
Kristiyan Kamburov <kkamburov@vmware.com>
2020-10-16 10:02:42 +0300

    module name fix

--- Build/VMware.PSDesiredStateConfiguration.TestRuns.ps1
+++ Build/VMware.PSDesiredStateConfiguration.TestRuns.ps1
@@ -1,5 +1,7 @@
 . (Join-Path $PSScriptRoot 'common.ps1')
 
+$moduleName = 'VMware.PSDesiredStateConfiguration'
+
 $coveragePercent = Start-UnitTests $moduleName
 
 $resultPath = Join-Path $env:TRAVIS_BUILD_DIR $env:PSDS_CODECOVERAGE_RESULTFILE
