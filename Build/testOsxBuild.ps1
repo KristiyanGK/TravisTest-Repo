8010e30cb858d9d74710363a43cab1252eeed780
Kristiyan <kristiangk@abv.bg>
2020-10-21 16:03:41 +0300

    tests only

--- Build/testOsxBuild.ps1
+++ Build/testOsxBuild.ps1
@@ -1,11 +1,5 @@
 . (Join-Path $PSScriptRoot 'common.ps1')
 
-# Updating the content of the psm1 and psd1 files via the build module file.
-$moduleName = 'VMware.vSphereDSC'
-$moduleRoot = Join-Path $Script:SourceRoot $moduleName
-$buildModuleFilePath = Join-Path -Path $moduleRoot -ChildPath "$moduleName.build.ps1"
-. $buildModuleFilePath
-
 # run tests and calculate coverage percent
 $coveragePercent = Invoke-UnitTests $ModuleName
 
